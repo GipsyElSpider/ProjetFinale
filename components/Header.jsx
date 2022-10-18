092604cb91c3ac2e4424159b1e65a85cb8913df6
Glennlg <glenn.legal@freyja.software>
2022-10-18 10:59:08 +0200

    add css

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,38 +1,38 @@
 import Link from 'next/link';
 const Header = ({ auth }) => {
     return (
-        <div className="w-full flex flex-col md:flex md:flex-row px-12 py-6 bg-discordGray justify-between">
+        <navbar /* id="header" */  className="w-full flex flex-col md:flex-row px-12 py-6 bg-discordGray justify-center md:justify-between">
             <Link href={"/"}>
                 <img className="h-12 w-12 mx-auto md:mx-0" src="../data/icon-naruto.svg" alt='logo site' />
             </Link>
             {auth ?
-                <div className='flex flex-col md:flex-row w-full mx-auto md:mx-0 md:w-1/3 xl:w-1/4 md:justify-end items-center'>
+                <div className='navbar'>
                     <Link href={"/logout"}>
-                        <button className='w-fit rounded-xl border px-4 py-2 bg-gray-300 font-bold md:mr-4 mb-2 md:mb-0'>
+                        <button className='navbarFirstBtn'>
                             Déconnection
                         </button>
                     </Link>
                     <Link href={`/myprofile`}>
-                        <button className='w-fit rounded-xl border px-4 py-2 bg-gray-300 font-bold'>
+                        <button className='navbarSecondBtn'>
                             {auth}
                         </button>
                     </Link>
                 </div>
                 :
-                <div className='flex w-1/4 justify-evenly'>
+                <div className='navbar'>
                     <Link href={"/login"}>
-                        <button className='rounded-xl border px-4 py-2 bg-gray-300 font-bold'>
+                        <button className='navbarFirstBtn'>
                             Connection
                         </button>
                     </Link>
                     <Link href={"/register"}>
-                        <button className='rounded-xl border px-4 py-2 bg-gray-300 font-bold'>
+                        <button className='navbarSecondBtn'>
                             Inscription
                         </button>
                     </Link>
                 </div>
             }
-        </div>
+        </navbar>
     )
 };
 
